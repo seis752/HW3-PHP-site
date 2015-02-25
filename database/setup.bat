6b048c3b1307ef89f713b94e376670169e2c3444
jmcdowell <jmcdowell@users.noreply.github.com>
2015-02-24 22:34:09 -0600

    Add combined SQL script (structure + data)

--- database/setup.bat
+++ database/setup.bat
@@ -1,10 +1,10 @@
 rem Generate the "seis752justin_db" database from scripts
-
 rem Update host/user/password as necessary for your environment
-mysql -h localhost -u root -vvv < structure.sql
-mysql -h localhost -u root -vvv < data.sql
 
 rem "create-database.sql" is "structure.sql" + "data.sql"
-rem mysql -h localhost -u root -vvv < create-database.sql
+mysql -h localhost -u root -vvv < create-database.sql
+
+rem mysql -h localhost -u root -vvv < structure.sql
+rem mysql -h localhost -u root -vvv < data.sql
 
 pause
