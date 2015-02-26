a109dedf1f9efb4162293d584e4a36d47edfa773
Lloyd Cledwyn <cledwyn@gmail.com>
2015-02-25 23:31:32 -0600

    Update searchresults.php

--- searchresults.php
+++ searchresults.php
@@ -1,14 +1,16 @@
 <?php
-	include_once('settings.php');
+  include_once('settings.php');
 ?>
 <ol>
-	<?php       
-		$result = mysql_query("SELECT * FROM users where name like '%$name%' limit 20");
-		while($row = mysql_fetch_array($result))
-		{
-			echo '<li>'.$row['name'] . " - " . $row['username'];
-			echo "</li>";
-		}     
-		mysql_close($con);
-	?>
+  <?php       
+    $name = $_GET['nameSearch'];
+    $result = mysql_query("SELECT * FROM users where name like '%$name%' limit 20");
+    while($row = mysql_fetch_array($result))
+    {
+      echo '<li>'.$row['name'] . " - " . $row['username'];
+      echo "</li>";
+    }     
+    mysql_close($con);
+?>
 </ol>
+<?php include_once('close.php'); ?>
