d92836e27524671535f77723ebd651f08beeca07
Lloyd Cledwyn <cledwyn@gmail.com>
2015-02-25 23:18:29 -0600

    Create searchresults.php

---
+++ searchresults.php
@@ -0,0 +1,14 @@
+<?php
+	include_once('settings.php');
+?>
+<ol>
+	<?php       
+		$result = mysql_query("SELECT * FROM users where name like '%$name%' limit 20");
+		while($row = mysql_fetch_array($result))
+		{
+			echo '<li>'.$row['name'] . " - " . $row['username'];
+			echo "</li>";
+		}     
+		mysql_close($con);
+	?>
+</ol>
