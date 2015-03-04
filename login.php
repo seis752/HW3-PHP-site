04b8c80d78b729de76cc91f3083b387f80620e4f
Mike Palmer <mwpalmer@hotmail.com>
2015-03-04 16:07:44 -0600

    HW5
    New files for HW#5.

--- login.php
+++ login.php
@@ -7,8 +7,8 @@
 		<div id="Sign-In">
 			<fieldset style="width:30%"><legend>LOG-IN HERE</legend>
 			<form method="POST" action="listMessages.php">
-				User <br><input type="text" name="user" size="40"><br>
-				Password <br><input type="password" name="pass" size="40"><br>
+				User Name <br><input type="text" name="user" size="40"><br>
+				Password  <br><input type="password" name="pass" size="40"><br>
 				<input id="button" type="submit" name="submit" value="Log-In">
 			</form>
 			</fieldset>
