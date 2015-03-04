566cbabb930f92b19f36c52d9d1ae88a79be8434
woodmanhu <woodmanhu@gmail.com>
2015-03-04 15:37:42 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'mysql.seis752.com');
-define('DB_USERNAME', 'seis752qiwen');
-define('DB_PASSWORD', '5eHDPTHnHn');
-define('DB_DATABASE', 'seis752qiwen_db');
+define('DB_USERNAME', 'DB_USERNAME');
+define('DB_PASSWORD', 'DB_PASSWORD');
+define('DB_DATABASE', 'DB_DATABASE');
 
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_DATABASE); 
 ?>
